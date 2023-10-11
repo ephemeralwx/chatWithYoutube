14f86c57931f5f067b5022cd9ee9f6897dd6fdb6
Kevin <kevinx8017@gmail.com>
2023-10-11 10:49:45 -0400

    changed to auto transcribe 10:49am

--- apirobustmain.py
+++ apirobustmain.py
@@ -76,6 +76,7 @@ if __name__ == '__main__':
 '''
 from flask import Flask, request, jsonify
 from transformers import DistilBertTokenizer, DistilBertForQuestionAnswering
+from youtube_transcript_api import YouTubeTranscriptApi
 
 import torch
 
@@ -83,25 +84,29 @@ app = Flask(__name__)
 
 # Load a fine-tuned BERT model and tokenizer
 MODEL_NAME = "distilbert-base-uncased-distilled-squad"
-
 tokenizer = DistilBertTokenizer.from_pretrained(MODEL_NAME)
 model = DistilBertForQuestionAnswering.from_pretrained(MODEL_NAME)
-
 
 # Optimize model for inference
 model.eval()
 
-# In-memory storage for video transcripts (in a real-world scenario, consider using databases like PostgreSQL, MongoDB, etc.)
+# In-memory storage for video transcripts
 video_data = {}
 
 @app.route('/store_transcript', methods=['POST'])
 def store_transcript():
     video_id = request.json.get('video_id')
-    transcript = request.json.get('transcript')
 
-    if not video_id or not transcript:
-        return jsonify({"error": "video_id or transcript missing"}), 400
-    
+    if not video_id:
+        return jsonify({"error": "video_id is missing"}), 400
+
+    # Fetching transcript from YouTube
+    try:
+        transcript_data = YouTubeTranscriptApi.get_transcript(video_id)
+        transcript = ' '.join([entry['text'] for entry in transcript_data])
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+
     video_data[video_id] = transcript
     return jsonify({"message": "Transcript stored successfully!"})
 
@@ -113,7 +118,6 @@ def ask():
     if not video_id or not query:
         return jsonify({"error": "video_id or query missing"}), 400
 
-    # Retrieve the video transcript
     transcript = video_data.get(video_id)
     if not transcript:
         return jsonify({"error": "No transcript found for the given video_id"}), 400
@@ -126,7 +130,6 @@ def ask():
         answer_start = torch.argmax(output.start_logits)
         answer_end = torch.argmax(output.end_logits)
 
-    # Extract the answer
     answer = tokenizer.decode(input_ids[answer_start:answer_end+1], skip_special_tokens=True)
 
     if answer:
